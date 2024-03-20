c27bf79a5cede926f2a7120c0f1b86ded914ff77
Mario Contreras <mariocc29@gmail.com>
2024-03-20 12:58:28 -0400

    Added footer section

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { StarshipComponent } from './starship/starship.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    StarshipComponent
+    StarshipComponent,
+    FooterComponent
   ],
   exports: [
     HeaderComponent,
-    StarshipComponent
+    StarshipComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
