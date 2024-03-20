9b565ad796cb967f54433833e00fdc35bc01e250
Mario Contreras <mariocc29@gmail.com>
2024-03-20 12:02:59 -0400

    added main section, and fixed unexpected behaviors on the layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { StarshipComponent } from './starship/starship.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    StarshipComponent
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    StarshipComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/shared/starship/starship.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-starship',
+  templateUrl: './starship.component.html',
+  styleUrl: './starship.component.scss'
+})
+export class StarshipComponent {
+
+}
